e4121bf319b1c39902391dc3f18829f4ac9b224e
Md Azad Hossain Tutul <mdazadhossain95@gmail.com>
2021-11-07 11:28:28 +0600

    updated debug

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Calculator(),
     );
   }
